90efa2912124f1356cc9f3775f659974c3bef0b3
rango198 <137562811+rango198@users.noreply.github.com>
2024-02-22 08:39:37 +0200

    route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
 import { PersistGate } from 'redux-persist/integration/react';
-
 import { store, persistor } from './redux/store';
 import App from 'components/App';
 import { Provider } from 'react-redux';
@@ -13,7 +11,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook2">
           <App />
         </BrowserRouter>
       </PersistGate>
